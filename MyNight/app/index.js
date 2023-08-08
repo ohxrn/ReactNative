55ee57fb6611507a3f0547bb461d784d504d1b4f
Jack Ohrn <jackohrn1@gmail.com>
2023-08-07 21:36:35 -0400

    updates

--- MyNight/app/index.js
+++ MyNight/app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Home = () => {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text>SKINBAHAHAHAHAHHAHAKERBONKER</Text>
     </View>
   );
 };
